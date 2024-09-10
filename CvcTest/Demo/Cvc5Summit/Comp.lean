969e481dd7e757094bc78d799b32e98f352333ce
Adrien Champion <adrien.champion@protonmail.com>
2024-09-10 15:08:33 +0200

    chore: comparison demo between lean-cvc5 and cvc.lean basic/safe (cvc5 summit)

---
+++ CvcTest/Demo/Cvc5Summit/Comp.lean
@@ -0,0 +1,144 @@
+import Cvc
+
+
+
+/-! # Comparison between `lean-cvc5`, `cvc.lean` basic, and `cvc.lean` safe(r) -/
+
+
+
+section
+open cvc5
+
+def cvc5Demo (tm : TermManager) : SolverT BaseIO (Option Term) := do
+  let intSort := tm.getIntegerSort
+
+  let three ← tm.mkInteger 3
+  let six ← tm.mkInteger 6
+
+  Solver.setLogic "QF_LIA"
+  Solver.setOption "produce-models" "true"
+
+  let n ← Solver.declareFun "n" #[] intSort true
+
+  -- `3 * n`
+  let three_n ← tm.mkTerm .MULT #[three, n]
+  -- `3 * n = 6`
+  let t ← tm.mkTerm .EQUAL #[three_n, six]
+
+  Solver.assertFormula t
+
+  let isSatRes ← Solver.checkSat
+
+  if isSatRes.isSat then
+    let nVal ← Solver.getValue n
+    return nVal
+  else
+    return none
+
+/-- info: ok: (some 2) -/
+#guard_msgs in
+#eval do
+  let tm ← TermManager.new
+  cvc5Demo tm |> Solver.run tm
+
+end
+
+
+
+section
+open Cvc
+
+open Smt in
+def cvcBasicDemo : SmtIO (Option Term) := do
+  let three ← Term.mkInt 3
+  let six ← Term.mkInt 6
+
+  setLogic Logic.qf_lia
+  setOption "produce-models" "true"
+
+  let n ← declareFun "n" Int
+
+  let three_n ← three.mult n
+  let t ← three_n.eq six
+
+  assert t
+
+  let isSat? ← checkSat?
+
+  if let some true := isSat? then
+    let nVal ← getValue n
+    return nVal
+  else
+    return none
+
+/-- info: 2 -/
+#guard_msgs in
+#eval do
+  if let some nVal ← cvcBasicDemo.run! then
+    return nVal
+  else
+    IO.throwServerError "expected sat result"
+
+end
+
+
+
+section
+open Cvc.Safe
+
+open Smt in
+def cvcSafeDemo : SmtT IO (Option Int) := do
+  let three ← Term.mkInt 3
+  let six ← Term.mkInt 6
+
+  setLogic .qf_lia
+  setOption "produce-models" "true"
+
+  let n ← declareFun "n" Int
+  let three_n ← three.mult n
+  let t ← three_n.eq six
+
+  assert t
+
+  checkSat
+    (ifSat := do
+      let nVal ← getValue n
+      return nVal)
+    (ifUnsat := return none)
+
+
+/-- info: 2 -/
+#guard_msgs in
+#eval do
+  if let some nVal ← cvcSafeDemo.run! then
+    return nVal
+  else
+    IO.throwServerError "expected sat result"
+
+
+
+open Smt in
+def cvcSafeDemo' : SmtT IO (Option Int) := do
+  setLogic .qf_lia
+  setOption "produce-models" "true"
+
+  let n ← declareFun "n" Int
+  let t ← smt! ((3 * n) = 6) ∧ ¬ (n ≤ 1)
+
+  assert t
+
+  checkSat
+    (ifSat := do
+      let nVal ← getValue n
+      return nVal)
+    (ifUnsat := return none)
+
+
+/-- info: 2 -/
+#guard_msgs in
+#eval do
+  if let some nVal ← cvcSafeDemo'.run! then
+    return nVal
+  else
+    IO.throwServerError "expected sat result"
+end
