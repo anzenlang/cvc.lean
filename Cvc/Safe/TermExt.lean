b397b995c79816ec3509e0d1e61392cf49cf8f1e
Adrien Champion <adrien.champion@protonmail.com>
2025-02-13 12:56:59 +0100

    chore: fix/update all copyright notices

--- Cvc/Safe/TermExt.lean
+++ Cvc/Safe/TermExt.lean
@@ -1,12 +1,5 @@
 /-
-Copyright (c) 2023-2024 by the authors listed in the file AUTHORS and their
-institutional affiliations. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Adrien Champion
--/
-
-/-
-Copyright (c) 2023-2024 by the authors listed in the file AUTHORS and their
+Copyright (c) 2023-2025 by the authors listed in the file AUTHORS and their
 institutional affiliations. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Adrien Champion
--- Cvc/Tactic.lean
+++ Cvc/Tactic.lean
@@ -1,1 +1,8 @@
+/-
+Copyright (c) 2023-2025 by the authors listed in the file AUTHORS and their
+institutional affiliations. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Adrien Champion
+-/
+
 import Cvc.Tactic.Basic
