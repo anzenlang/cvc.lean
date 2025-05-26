ef53cc5728cad18d20e10e7b7ef21021ac65b261
Adrien Champion <adrien.champion@protonmail.com>
2025-05-26 15:29:56 +0200

    chore: compile-fail test tweaks

--- Tests/Terms/CompileFail.lean
+++ Tests/Terms/CompileFail.lean
@@ -15,7 +15,6 @@ namespace Cvc.Test
 
 Term.test!
 
-
 [Term.mkEqual.noArg]
   Term.mkEqual #[]
 /-- error: could not synthesize default value for parameter 'h_size' using tactics
@@ -26,7 +25,6 @@ error: expected an array of **at least** two terms
 
 [Term.mkDistinct.oneArg]
   let seven ← Term.int 7
-  -- all of type `Term Bool`
   Term.mkEqual #[seven]
 /--
 error: could not synthesize default value for parameter 'h_size' using tactics
@@ -36,30 +34,83 @@ seven : Term.Int
 ⊢ 2 ≤ #[seven].size
 -/
 
-[Term.mkAdd.nonArith]
+Term.test!
+
+with
   let b1 ← Term.bool true
   let b2 ← Term.bool false
   let b3 ← Term.bool false
   let b4 ← Term.bool false
   -- all of type `Term Bool`
-  Term.mkAdd #[b1, b2, b3, b4]
+
+[Term.mkMul.nonArith]
+  let _bad1 ← Term.mkMul #[b1, b2, b3, b4]
+  let _bad2 ← b1.mul b2
 /--
-error: could not synthesize default value for parameter 'h_arith' using tactics
+error: could not synthesize default value for parameter 'Arith' using tactics
 ---
 error: expected arithmetic type `Int` or `Rat`, see `Cvc.is_arith` and `Cvc.Srt.Bij.Arith`
 b1 b2 b3 b4 : Term.Bool
-⊢ Cvc.is_arith Bool
--/
-
-[Term.add.nonArith]
-  let b1 ← Term.bool true
-  let b2 ← Term.bool false
-  -- all of type `Term Bool`
-  b1.add b2
-/--
-error: could not synthesize default value for parameter 'h_arith' using tactics
+⊢ Srt.Bij.Arith Bool
+---
+error: could not synthesize default value for parameter 'Arith' using tactics
 ---
 error: expected arithmetic type `Int` or `Rat`, see `Cvc.is_arith` and `Cvc.Srt.Bij.Arith`
-b1 b2 : Term.Bool
-⊢ Cvc.is_arith Bool
+b1 b2 b3 b4 : Term.Bool
+_bad1 : Term Bool
+⊢ Srt.Bij.Arith Bool
+-/
+
+[Term.mkAdd.nonArith]
+  let _bad1 ← Term.mkAdd #[b1, b2, b3, b4]
+  let _bad2 ← b1.add b2
+/--
+error: could not synthesize default value for parameter 'Arith' using tactics
+---
+error: expected arithmetic type `Int` or `Rat`, see `Cvc.is_arith` and `Cvc.Srt.Bij.Arith`
+b1 b2 b3 b4 : Term.Bool
+⊢ Srt.Bij.Arith Bool
+---
+error: could not synthesize default value for parameter 'Arith' using tactics
+---
+error: expected arithmetic type `Int` or `Rat`, see `Cvc.is_arith` and `Cvc.Srt.Bij.Arith`
+b1 b2 b3 b4 : Term.Bool
+_bad1 : Term Bool
+⊢ Srt.Bij.Arith Bool
+-/
+
+[Term.mkDiv!.nonArith]
+  let _bad1 ← Term.mkDiv! #[b1, b2, b3, b4]
+  let _bad2 ← b1.div! b2
+/--
+error: could not synthesize default value for parameter 'Arith' using tactics
+---
+error: expected arithmetic type `Int` or `Rat`, see `Cvc.is_arith` and `Cvc.Srt.Bij.Arith`
+b1 b2 b3 b4 : Term.Bool
+⊢ Srt.Bij.Arith Bool
+---
+error: could not synthesize default value for parameter 'Arith' using tactics
+---
+error: expected arithmetic type `Int` or `Rat`, see `Cvc.is_arith` and `Cvc.Srt.Bij.Arith`
+b1 b2 b3 b4 : Term.Bool
+_bad1 : Term Bool
+⊢ Srt.Bij.Arith Bool
+-/
+
+[Term.mkDivTotal.nonArith]
+  let _bad1 ← Term.mkDivTotal b1 b2
+  let _bad2 ← b1.divTotal b2
+/--
+error: could not synthesize default value for parameter 'Arith' using tactics
+---
+error: expected arithmetic type `Int` or `Rat`, see `Cvc.is_arith` and `Cvc.Srt.Bij.Arith`
+b1 b2 b3 b4 : Term.Bool
+⊢ Srt.Bij.Arith Bool
+---
+error: could not synthesize default value for parameter 'Arith' using tactics
+---
+error: expected arithmetic type `Int` or `Rat`, see `Cvc.is_arith` and `Cvc.Srt.Bij.Arith`
+b1 b2 b3 b4 : Term.Bool
+_bad1 : Term Bool
+⊢ Srt.Bij.Arith Bool
 -/
